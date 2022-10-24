b445ec36183cabfcadb98733f62fcd5b5f842648
belenazas <lbelen.azas@gmail.com>
2022-10-24 20:31:10 -0300

    Initial Commit

--- faq.js
+++
@@ -1,7 +0,0 @@
-/* Preguntas desplegables del FAQ */
-const faqs = document.querySelectorAll(".faq");
-faqs.forEach(faq=> {
-    faq.addEventListener("click", ()=> {
-        faq.classList.toggle("active")
-    })
-})
